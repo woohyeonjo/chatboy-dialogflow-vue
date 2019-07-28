9bf05c74ba8d495b06adc0f24d32c4344a4c937e
nnm <nnm@nnmui-MacBookPro.local>
2019-07-28 19:26:07 +0900

    time-stamp margin을 padding으로 변경

--- src/components/chatboy/ChatTextArea.vue
+++ src/components/chatboy/ChatTextArea.vue
@@ -16,7 +16,6 @@
 
 <script>
     import { mapGetters, mapMutations } from "vuex";
-
     export default {
         name: "ChatTextArea",
         methods: {
@@ -26,17 +25,6 @@
                     mainTextArea.scrollTop = mainTextArea.scrollHeight;
                 }, 0);
             },
-            //TODO: 시간 변환 메서드 작성
-            // getTime(chatDate) {
-            //     console.log(chatDate);
-            //     const date = Date.parse(chatDate);
-            //     console.log(date);
-            //     const hours =
-            //     const minutes = date.getMinutes();
-            //     const time = `${hours}:${minutes}`;
-            //
-            //     return time;
-            // }
         },
         computed: {
             ...mapGetters(['storedChatLog'])
@@ -48,75 +36,73 @@
 </script>
 
 <style scoped>
-.text-area {
-    position: absolute;
-    background-color: #9bbbd4;
-    top: 67px;
-    height: 253px;
-    width: 100%;
-    overflow-y: scroll;
-    -ms-overflow-style: none;
-}
-.text-area::-webkit-scrollbar {
-    display: none;
-}
-.message {
-    width: 85%;
-    margin: 5px auto;
-    margin-left: 65px;
-    height: auto;
-    overflow: hidden;
-}
-.message p {
-    position: relative;
-    display: block;
-    text-align: center;
-    padding: 13px;
-    font-size: 17px;
-    font-weight: 400;
-    border-radius: 10px;
-    margin: 10px;
-}
-.message p.to {
-    color: #7C7C7C;
-    background-color: #fef01b;
-    color: black;
-    float: right;
-}
-.message p.from {
-    color: white;
-    background-color: #ffffff;
-    color: white;
-    float: left;
-}
-.time-stamp.to {
-     position: absolute;
-     color: #556677;
-     font-size: 10px;
-     bottom: 2px;
-     left: -37px;
-}
-.time-stamp.from {
-    position: absolute;
-    color: #556677;
-    font-size: 10px;
-}
-.tail {
-    position: absolute;
-    bottom: 8px;
-    width: 10px;
-    height: 10px;
-}
-.tail.from {
-    left: -10px;
-    border-bottom: 10px solid #ffffff;
-    border-left: 10px solid transparent;
-    border-top-left-radius: 10px;
-}
-.tail.to {
-    right: -10px;
-    border-bottom: 10px solid #fef01b;
-    border-right: 10px solid transparent;
-    border-top-right-radius: 10px;
-}
+    .text-area {
+        position: absolute;
+        background-color: #9bbbd4;
+        top: 67px;
+        height: 253px;
+        width: 100%;
+        overflow-y: scroll;
+        -ms-overflow-style: none;
+    }
+    .text-area::-webkit-scrollbar {
+        display: none;
+    }
+    .message {
+        width: 90%;
+        padding: 5px 0 5px 40px;
+        height: auto;
+        overflow: hidden;
+    }
+    .message p {
+        position: relative;
+        display: block;
+        text-align: left;
+        word-break: break-all;
+        padding: 13px;
+        margin: 10px;
+        font-size: 17px;
+        font-weight: 400;
+        border-radius: 10px;
+    }
+    .message p.to {
+        background-color: #fef01b;
+        color: black;
+        float: right;
+    }
+    .message p.from {
+        color: white;
+        background-color: #ffffff;
+        float: left;
+    }
+    .time-stamp.to {
+        position: absolute;
+        color: #556677;
+        font-size: 10px;
+        bottom: 2px;
+        left: -37px;
+    }
+    .time-stamp.from {
+        position: absolute;
+        color: #556677;
+        font-size: 10px;
+    }
+    .tail {
+        position: absolute;
+        bottom: 8px;
+        width: 10px;
+        height: 10px;
+    }
+    .tail.from {
+        left: -10px;
+        border-bottom: 10px solid #ffffff;
+        border-left: 10px solid transparent;
+        border-top-left-radius: 10px;
+    }
+    .tail.to {
+        right: -10px;
+        border-bottom: 10px solid #fef01b;
+        border-right: 10px solid transparent;
+        border-top-right-radius: 10px;
+    }
 </style>
